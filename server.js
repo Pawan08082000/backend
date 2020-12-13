ca5e067d7a02b5eb8f0e024d3273810373446046
Pawan08082000 <59208833+Pawan08082000@users.noreply.github.com>
2020-12-13 17:13:40 +0530

    Routing and redirecting requests

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const http = require('http')
+const fs = require('fs')
 
-const server = http.createServer((req,res) => {
+
+const server = http.createServer((req, res) => {
     // console.log('req.url : ',req.url,' req.method : ',req.method,' req.headers : ', req.headers)
     // process.exit() //used for closing the server
-    res.setHeader('content-Type', 'text/html')
-    res.write(`<html>
+    let url = req.url
+    let method = req.method
+    if (url === '/') {
+        res.setHeader('content-Type', 'text/html')
+        res.write(`<html>
     <head>
-    <title>Pawan Tiwari</title></head></html>`)
+    <title>Pawan Tiwari</title></head>
+    <body>
+    <form action="/message" method="POST">
+    <input type='text' name='message'>
+    <button type='submit'>send</button>
+    </form></body></html>`)
+    }
+    if(method === 'POST' && url ==='/message'){
+        fs.writeFileSync('message.txt', 'DUmmy Data')
+        res.statusCode = 302;
+        res.setHeader('Location','/')
+        return res.end()
+    }
 });
 
 server.listen(9000)
