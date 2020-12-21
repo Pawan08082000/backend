6401c2e774ded800eac30c89f6a22a3dc75815f3
Pawan08082000 <59208833+Pawan08082000@users.noreply.github.com>
2020-12-22 05:18:01 +0530

    Added body-parser for parsing incoming data

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-const http = require('http')
-
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log('In the middleware')
-    next() //Allow the request to continue to the next middleware in line
-})
+app.use(bodyParser.urlencoded({extended:false}))
 
-app.use((req, res, next) => {
+app.use('/add-product',(req, res, next) => {
     console.log('In another middleware')
+    res.send(`<html>
+    <head>
+    <title>Pawan Tiwari</title></head>
+    <body>
+    <form action="/product" method="POST">
+    <input type='text' name='message'>
+    <button type='submit'>send</button>
+    </form></body></html>`)
 })
-const server = http.createServer(app);
 
-server.listen(9000)
+app.use('/product',(req, res, next) => {
+    console.log(req.body)
+    res.redirect('/') //Allow the request to continue to the next middleware in line
+})
+
+app.listen(9000)
