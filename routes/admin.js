cf3b141d3a7680ee01c7a6318a7a46210d73c91e
Pawan08082000 <59208833+Pawan08082000@users.noreply.github.com>
2020-12-26 18:57:43 +0530

    Filtering Paths According to file location

--- routes/admin.js
+++ routes/admin.js
@@ -2,21 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => get
 router.get('/add-product',(req, res, next) => {
     console.log('In another middleware')
     res.send(`<html>
     <head>
     <title>Pawan Tiwari</title></head>
     <body>
-    <form action="/product" method="POST">
+    <form action="/admin/add-product" method="POST">
     <input type='text' name='message'>
     <button type='submit'>send</button>
     </form></body></html>`)
 })
 
-router.post('/product',(req, res, next) => {
+// /admin/add-product => post
+router.post('/add-product',(req, res, next) => {
     console.log(req.body)
-    res.redirect('/') //Allow the request to continue to the next middleware in line
+    res.redirect('/shop/shop') //Allow the request to continue to the next middleware in line
 })
 
 module.exports =  router;
--- server.js
+++ server.js
@@ -11,8 +11,12 @@ const shopRoute = require('./routes/shop');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use(adminRoute);
-app.use(shopRoute);
+app.use('/admin',adminRoute);
+app.use('/shop',shopRoute);
+
+app.use((req,res,next)=>{
+    res.status(404).send(`<h1>Page not found!</h1>`);
+})
 
 var server = app.listen(9000,function(){
     var port = server.address().port;
